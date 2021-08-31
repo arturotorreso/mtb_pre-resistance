6651c8452cb00901dd4d57f429535e95999eef59
Arturo <arturotorres91@gmail.com>
2021-08-31 13:05:21 -0700

    Add files via upload

---
+++ run_bactDat.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+#$ -N bactdating_raxml
+#$ -l h_vmem=10G
+#$ -l tmem=10G
+#$ -l h_rt=5000:0:0
+#$ -j y
+#$ -t 1-3
+#$ -l tscratch=3G
+#$ -o /
+
+date
+hostname
+
+lin=l4
+OUT_DIR=/$lin.wg;
+IN_DIR=/$lin.wg;
+IN_FILE=$IN_DIR/$lin.assembly.wg.raxml.dates.tree;
+
+LENGTH=4411532
+
+MODEL=mixedgamma
+ITER=1e7
+RATE=0.5
+
+PREFIX=$lin.$MODEL.$ITER.$RATE.wg${SGE_TASK_ID}
+
+
+Rscript --vanilla /SAN/ballouxlab/tb_lima/dev/runBactDat.R -i $IN_FILE -p $PREFIX -d $OUT_DIR -l $LENGTH -m $MODEL -n $ITER -s TRUE -M $RATE > $OUT_DIR/${PREFIX}_simplified.txt
+
+date
+hostname
